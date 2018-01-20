904e7ca4d6c8553197fa26b982089841357d3bd2
Abel Corchero <abelcc1995@hotmail.com>
2018-01-21 00:53:51 +0100

    Oferta

---
+++ Comerç Autoservei/Oferta.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Oferta.h
+ * Author: Abel
+ *
+ * Created on 21 de enero de 2018, 0:46
+ */
+
+#ifndef OFERTA_H
+#define OFERTA_H
+
+class Oferta {
+public:
+    Oferta();
+    Oferta(const Oferta& orig);
+    virtual ~Oferta();
+private:
+
+};
+
+#endif /* OFERTA_H */
+
